1ea5ab2b8d5657053c99118fc752d8438c77743d
khalid <khalid.a.montes@gmail.com>
2023-02-06 20:39:46 -0500

    commit tree 602c4f18a24b0d65d8b3feae1e70ac6badff91cf

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your code here
+ function submitData (name, email) {
+    let formData = {
+        name: name,
+        email: email 
+    };
+}
+
+configurationObj = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application.json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(formData)
+};
